959157f01a235bdb0cf327fa999e6ed22bfa62b5
shunyihuang1997 <43654798+shunyihuang1997@users.noreply.github.com>
2022-09-06 19:00:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import numpy as np
 from flask import Flask, request,render_template
 import pickle
+from collections.abc import Mapping
+from collections.abc import MutableMapping
+from collections.abc import Sequence
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
